a2985800d939918618ea5ba8f409e36a85285763
陈斌 <chenbkeep@163.com>
2017-01-20 19:56:40 +0800

    抽取ACE中文语料中所有事件，共2521个

--- acedeal/main.py
+++ acedeal/main.py
@@ -5,28 +5,18 @@ Created on 2017年1月19日
 @author: chenbin
 '''
 
-from acedeal.pre_process_ace import extract_info
+from acedeal.pre_process_ace import get_ace_event_list, save_ace_event_list
 
 
 if __name__ == "__main__":
     
     save_path = "./ch.txt"
-    apf_file_path = "../ace05/data/Chinese/nw/adj/XIN20001002.0200.0004.apf.xml"
-    sgm_file_path = "../ace05/data/Chinese/nw/adj/XIN20001002.0200.0004.sgm"
+    ace_file_path = "../ace05/data/Chinese/"
+#     ace_file_path = "../ace05/data/Chinese/nw/adj/XIN20001002.0200.0004.apf.xml"
+#     sgm_file_path = "../ace05/data/Chinese/nw/adj/XIN20001002.0200.0004.sgm"
     
-    ace_info_list=extract_info(apf_file_path,sgm_file_path)
+    ace_list=get_ace_event_list(ace_file_path)
     
-    for ace_info in ace_info_list:
-        ace_info.toString()
+    save_ace_event_list(ace_list,save_path)
     
-#     language = "ch"  # you can change en to "ch" if the input in a chinese data set
-# 
-#     # First-Part: extraction ACE mention from raw dataset
-#     # to gain all the mention from files , you should change the file name by yourself
-#     other_save_path = "./ch.txt"
-#     # mention_save_path = "out/ace2005/en_wl.txt"
-#     directory = "../../ace05/data/Chinese/bn/adj"
-# 
-#     ace_parse = ACE_parse(other_save_path, directory, language)
-#     ace_parse.get_other()
-#     ace_parse.get_info()
+    
--- acedeal/pre_process_ace.py
+++ acedeal/pre_process_ace.py
@@ -4,9 +4,16 @@ Created on 2017年1月19日
 
 @author: chenbin
 '''
+from setuptools.sandbox import save_path
+
+'''
+ACE  event实体
+'''
 
 from acedeal.xml_parse import xml_parse_base
+import os
 from xml.dom import minidom
+
 
 class ACE_info:
     # go gain the event mention from ACE dataset
@@ -17,34 +24,76 @@ class ACE_info:
         self.sub_type = None  # sub-type of this event
         
     def toString(self):
-        print('id:'+str(self.id)+'\t text:'+str(self.text)+'\t trigger:'+str(self.trigger)+'\t sub_type:'+str(self.sub_type))
+        return 'id:'+str(self.id)+'\t text:'+str(self.text)+'\t trigger:'+str(self.trigger)+'\t sub_type:'+str(self.sub_type)
 
 
-def extract_info(apf_name,sgm_name):
+'''
+抽取单个apf.xml中的事件实体ACE_info
+'''
+def extract_ace_info(apf_file):
     
+    # 存储事件实体的list
     R=[]
     
-    doc = minidom.parse(apf_name)
+    doc = minidom.parse(apf_file)
     root = doc.documentElement
     
     event_nodes = xml_parse_base.get_xmlnode(None,root, 'event')
     for node in event_nodes:
         R_element = ACE_info()
+        # 获取事件id
         R_element.id=xml_parse_base.get_attrvalue(None, node, 'ID')
+        # 获取事件子类型
         R_element.sub_type = xml_parse_base.get_attrvalue(None,node, 'SUBTYPE')
+        #获取事件mention
         mention_nodes = xml_parse_base.get_xmlnode(None,node, 'event_mention')
         for mention_node in mention_nodes:
+            # 获取事件所在语句
             mention_ldc_scope=xml_parse_base.get_xmlnode(None,mention_node, 'ldc_scope')
             mention_ldc_scope_charseq=xml_parse_base.get_xmlnode(None,mention_ldc_scope[0], 'charseq')
             R_element.text=xml_parse_base.get_nodevalue(None,mention_ldc_scope_charseq[0],0).replace("\n", "")
             
+            # 获取事件触发词
             mention_anchor=xml_parse_base.get_xmlnode(None,mention_node, 'anchor')
             mention_anchor_charseq=xml_parse_base.get_xmlnode(None,mention_anchor[0], 'charseq')
-            R_element.trigger=xml_parse_base.get_nodevalue(None,mention_anchor_charseq[0],0)
+            R_element.trigger=xml_parse_base.get_nodevalue(None,mention_anchor_charseq[0],0).replace("\n", "")
             
         R.append(R_element)
         
     return R
+
+
+'''
+抽取整个ace语料中的所有事件
+'''
+def get_ace_event_list(ace_file_path):
+    ace_list=[]
+    
+    for filename in os.listdir(ace_file_path):
+        # adj文件夹所在地
+        adj_file_path=os.path.join(ace_file_path,filename,'adj')
+        for apf_file in os.listdir(adj_file_path):
+            # 获取.apf.xml的文件
+            if ".apf.xml" in apf_file:
+                # apf文件
+                apf_file_path=os.path.join(adj_file_path,apf_file)
+                ace_info_list=extract_ace_info(apf_file_path)
+                ace_list.extend(ace_info_list)
+                
+    return ace_list
+
+
+'''
+保存ace事件到txt
+'''
+def save_ace_event_list(ace_list,save_path):
+    f_out = open(save_path, 'w',encoding="utf-8")
+    for ace_info in ace_list:
+        f_out.write(ace_info.toString())
+        f_out.write('\n')
+        
+    f_out.close()
+
 #         R = []
 #         doc = minidom.parse(file_name)
 #         root = doc.documentElement
