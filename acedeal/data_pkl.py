1656ebb143bb2e5b6d3070b0c4a0076698677c09
陈斌 <chenbkeep@163.com>
2017-02-06 17:27:59 +0800

    lstm处理事件抽取

---
+++ acedeal/data_pkl.py
@@ -0,0 +1,232 @@
+'''
+Created on 2017年2月6日
+
+@author: chenbin
+'''
+# 使用pickle模块将数据对象保存到文件
+
+# import pickle
+# coding:utf-8
+'''
+Created on 2017年1月24日
+lstm
+@author: chenbin
+'''
+
+from gensim.models import word2vec
+from acedeal.nlpir import *
+from acedeal.pre_process_ace import *
+
+import tensorflow as tf
+import numpy as np
+import pickle
+
+
+ace_type_dict = {
+    'Be-Born': 0,
+    'Die': 1,
+    'Marry': 2,
+    'Divorce': 3,
+    'Injure': 4,
+    'Transfer-Ownership': 5,
+    'Transfer-Money': 6,
+    'Transport': 7,
+    'Start-Org': 8,
+    'End-Org': 9,
+    'Declare-Bankruptcy': 10,
+    'Merge-Org': 11,
+    'Attack': 12,
+    'Demonstrate': 13,
+    'Meet': 14,
+    'Phone-Write': 15,
+    'Start-Position': 16,
+    'End-Position': 17,
+    'Nominate': 18,
+    'Elect': 19,
+    'Arrest-Jail': 20,
+    'Release-Parole': 21,
+    'Charge-Indict': 22,
+    'Trial-Hearing': 23,
+    'Sue': 24,
+    'Convict': 25,
+    'Sentence': 26,
+    'Fine': 27,
+    'Execute': 28,
+    'Extradite': 29,
+    'Acquit': 30,
+    'Pardon': 31,
+    'Appeal': 32,
+    'None-role': 33
+}
+
+if __name__ == "__main__":
+    print("-----------------------start----------------------")
+
+    model = word2vec.Word2Vec.load_word2vec_format(
+        "./corpus_deal/ace_train_corpus.bin", binary=True)
+
+    ace_train_path = "../ace_experiment/test/"
+    ace_train_list = get_ace_event_list(ace_train_path)
+    ace_train_list_len = len(ace_train_list)
+    text_list = []
+    trigger_list = []
+    trigger_temp_list = []
+    type_list = []
+    type_temp_list = []
+
+    for i in range(ace_train_list_len):
+        ace_info = ace_train_list[i]
+        if i < ace_train_list_len - 1:
+
+            if ace_info.text == ace_train_list[i + 1].text:
+                trigger_temp_list.append(ace_info.trigger)
+                type_temp_list.append(ace_info.sub_type)
+            else:
+                text_list.append(ace_info.text)
+                trigger_temp_list.append(ace_info.trigger)
+                trigger_list.append(trigger_temp_list)
+                trigger_temp_list = []
+                type_temp_list.append(ace_info.sub_type)
+                type_list.append(type_temp_list)
+                type_temp_list = []
+        else:
+            trigger_temp_list.append(ace_info.trigger)
+            trigger_list.append(trigger_temp_list)
+            type_temp_list.append(ace_info.sub_type)
+            type_list.append(type_temp_list)
+            text_list.append(ace_info.text)
+
+#     print(len(text_list))
+#     print(len(trigger_list))
+#     print(len(type_list))
+
+    trigger_temp_list = []
+    type_temp_list = []
+    text_list_len = len(text_list)
+
+    ace_data = []
+    ace_data_labels = []
+
+    for i in range(text_list_len):
+        event_text = text_list[i]
+        trigger_temp_list = trigger_list[i]
+        type_temp_list = type_list[i]
+
+        sentence_word2vec_arr = []  # 句子
+        trigger_labels = []
+
+        ace_text_list = NLPIR_ParagraphProcess(event_text, 0).split(' ')
+        for word in ace_text_list:
+            # 读取词向量，如果没有该单词，则None
+            try:
+                word_vector = model[word]
+            except KeyError:
+                word_vector = None
+
+            #
+            if word_vector is not None:
+                sentence_word2vec_arr.append(word_vector)
+                if word in trigger_temp_list:
+                    val = ace_type_dict[
+                        type_temp_list[trigger_temp_list.index(word)]]
+                    a = [0.0 for x in range(0, 34)]
+                    a[val] = 1.0
+                    trigger_labels.append(a)
+                else:
+                    val = ace_type_dict['None-role']
+                    a = [0.0 for x in range(0, 34)]
+                    a[val] = 1.0
+                    trigger_labels.append(a)
+
+        ace_data.append(np.array(sentence_word2vec_arr))
+        ace_data_labels.append(trigger_labels)
+
+    ace_data_pkl = open('./corpus_deal/ace_data/ace_data_test.pkl', 'wb')
+    pickle.dump(ace_data, ace_data_pkl)
+    ace_data_pkl.close()
+
+    ace_data_labels_pkl = open(
+        './corpus_deal/ace_data/ace_data_test_labels.pkl', 'wb')
+    pickle.dump(ace_data_labels, ace_data_labels_pkl)
+    ace_data_labels_pkl.close()
+
+#     # 取出每一个事件实体
+#     for ace_info in ace_train_list:
+#
+#         if ace_info.text not in text_list:
+#             if trigger_temp_list:
+#                 pass
+#             else:
+#                 trigger_temp_list.append(ace_info.trigger)
+#             trigger_list.append(trigger_temp_list)
+#             trigger_temp_list = []
+#             text_list.append(ace_info.text)
+#         else:
+#             trigger_temp_list.append(ace_info.trigger)
+#
+#         print(text_list)
+#         print(trigger_list)
+#         print('-------------------------------')
+
+#         # if
+#         ace_text_list = NLPIR_ParagraphProcess(ace_info.text, 0).split(' ')
+#         for word in ace_text_list:
+#             # 读取词向量，如果没有该单词，则None
+#             try:
+#                 word_vector = model[word]
+#             except KeyError:
+#                 word_vector = None
+#
+#             #
+#             if word_vector is not None:
+#                 pass
+#             else:
+#                 print(word)
+
+    # print(ace_text_list)
+    # print(ace_info.toString())
+
+    print("-----------------------end----------------------")
+
+
+'''
+创建pkl文件
+'''
+#
+# data1 = {'a': [1, 2.0, 3, 4 + 6j],
+#          'b': ('string', u'Unicode string'),
+#          'c': None}
+#
+# selfref_list = [1, 2, 3]
+# selfref_list.append(selfref_list)
+#
+# output = open('data.pkl', 'wb')
+#
+# # Pickle dictionary using protocol 0.
+# pickle.dump(data1, output)
+#
+# # Pickle the list using the highest protocol available.
+# pickle.dump(selfref_list, output, -1)
+#
+# output.close()
+
+
+'''
+读取pkl文件
+'''
+# 使用pickle模块从文件中重构python对象
+
+# import pprint
+# import pickle
+#
+# data_file = open('./corpus_deal/ace_data.pkl', 'rb')
+#
+# data1 = pickle.load(data_file)
+# pprint.pprint(data1)
+#
+# labels_file = open('./corpus_deal/ace_data_labels.pkl', 'rb')
+# data2 = pickle.load(labels_file)
+# pprint.pprint(data2)
+#
+# data_file.close()
+# labels_file.close()
--- acedeal/lstm.py
+++ acedeal/lstm.py
@@ -1,177 +1,275 @@
 # coding:utf-8
+
 '''
-Created on 2017年1月24日
-lstm
+Created on 2017年2月6日
+
 @author: chenbin
 '''
 
-from gensim.models import word2vec
-from acedeal.nlpir import *
-from acedeal.pre_process_ace import *
-
+import pprint
+import pickle
 import tensorflow as tf
 import numpy as np
+import sys
+
+# 数据读取，训练集和测试集
+ace_data_train_file = open('./corpus_deal/ace_data/ace_data_train.pkl', 'rb')
+ace_data_train = pickle.load(ace_data_train_file)
+
+ace_data_train_labels_file = open(
+    './corpus_deal/ace_data/ace_data_train_labels.pkl', 'rb')
+ace_data_train_labels = pickle.load(ace_data_train_labels_file)
+
+ace_data_test_file = open('./corpus_deal/ace_data/ace_data_test.pkl', 'rb')
+ace_data_test = pickle.load(ace_data_test_file)
+
+ace_data_test_labels_file = open(
+    './corpus_deal/ace_data/ace_data_test_labels.pkl', 'rb')
+ace_data_test_labels = pickle.load(ace_data_test_labels_file)
+
+ace_data_train_file.close()
+ace_data_train_labels_file.close()
+ace_data_test_file.close()
+ace_data_test_labels_file.close()
 
 
-ace_type_dict = {
-    'Be-Born': 0,
-    'Die': 1,
-    'Marry': 2,
-    'Divorce': 3,
-    'Injure': 4,
-    'Transfer-Ownership': 5,
-    'Transfer-Money': 6,
-    'Transport': 7,
-    'Start-Org': 8,
-    'End-Org': 9,
-    'Declare-Bankruptcy': 10,
-    'Merge-Org': 11,
-    'Attack': 12,
-    'Demonstrate': 13,
-    'Meet': 14,
-    'Phone-Write': 15,
-    'Start-Position': 16,
-    'End-Position': 17,
-    'Nominate': 18,
-    'Elect': 19,
-    'Arrest-Jail': 20,
-    'Release-Parole': 21,
-    'Charge-Indict': 22,
-    'Trial-Hearing': 23,
-    'Sue': 24,
-    'Convict': 25,
-    'Sentence': 26,
-    'Fine': 27,
-    'Execute': 28,
-    'Extradite': 29,
-    'Acquit': 30,
-    'Pardon': 31,
-    'Appeal': 32,
-    'None-role': 33
+# RNN学习时使用的参数
+learning_rate = 0.001  # 1
+training_iters = len(ace_data_train)
+batch_size = 1
+display_step = 10
+
+# 神经网络的参数
+n_input = 200  # 输入层的n
+n_steps = 1  # 28长度
+n_hidden = 128  # 隐含层的特征数
+n_classes = 34  # 输出的数量，因为是分类问题，0~9个数字，这里一共有10个
+
+# 构建tensorflow的输入X的placeholder
+x = tf.placeholder("float", [None, n_steps, n_input])
+# tensorflow里的LSTM需要两倍于n_hidden的长度的状态，一个state和一个cell
+# Tensorflow LSTM cell requires 2x n_hidden length (state & cell)
+istate = tf.placeholder("float", [None, 2 * n_hidden])
+# 输出Y
+y = tf.placeholder("float", [None, n_classes])  # 2
+
+# 随机初始化每一层的权值和偏置
+weights = {
+    # Hidden layer weights
+    'hidden': tf.Variable(tf.random_normal([n_input, n_hidden])),
+    'out': tf.Variable(tf.random_normal([n_hidden, n_classes]))
+}
+biases = {
+    'hidden': tf.Variable(tf.random_normal([n_hidden])),
+    'out': tf.Variable(tf.random_normal([n_classes]))
 }
 
-if __name__ == "__main__":
-    print("-----------------------start----------------------")
+'''
+构建RNN
+'''
 
-    model = word2vec.Word2Vec.load_word2vec_format(
-        "./corpus_deal/ace_train_corpus.bin", binary=True)
 
-    ace_train_path = "../ace_experiment/train/"
-    ace_train_list = get_ace_event_list(ace_train_path)
-    ace_train_list_len = len(ace_train_list)
-    text_list = []
-    trigger_list = []
-    trigger_temp_list = []
-    type_list = []
-    type_temp_list = []
+def RNN(_X, _istate, _weights, _biases):
+    # 规整输入的数据
+    _X = tf.transpose(_X, [1, 0, 2])  # permute n_steps and batch_size    #3
 
-    for i in range(ace_train_list_len):
-        ace_info = ace_train_list[i]
-        if i < ace_train_list_len - 1:
+    _X = tf.reshape(_X, [-1, n_input])  # (n_steps*batch_size, n_input)
+    # 输入层到隐含层，第一次是直接运算
+    _X = tf.matmul(_X, _weights['hidden']) + _biases['hidden']
+    # 之后使用LSTM
+    lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(
+        n_hidden, forget_bias=1.0, state_is_tuple=False)
+#     lstm_cell = rnn_cell.BasicLSTMCell(n_hidden, forget_bias=1.0, state_is_tuple=False)
+    # 28长度的sequence，所以是需要分解位28次
+    _X = tf.split(0, n_steps, _X)  # n_steps * (batch_size, n_hidden)    #4
+    # 开始跑RNN那部分
+    outputs, states = tf.nn.rnn(lstm_cell, _X, initial_state=_istate)
 
-            if ace_info.text == ace_train_list[i + 1].text:
-                trigger_temp_list.append(ace_info.trigger)
-                type_temp_list.append(ace_info.sub_type)
-            else:
-                text_list.append(ace_info.text)
-                trigger_temp_list.append(ace_info.trigger)
-                trigger_list.append(trigger_temp_list)
-                trigger_temp_list = []
-                type_temp_list.append(ace_info.sub_type)
-                type_list.append(type_temp_list)
-                type_temp_list = []
-        else:
-            trigger_temp_list.append(ace_info.trigger)
-            trigger_list.append(trigger_temp_list)
-            type_temp_list.append(ace_info.sub_type)
-            type_list.append(type_temp_list)
-            text_list.append(ace_info.text)
+    # 输出层
+    return tf.matmul(outputs[-1], _weights['out']) + _biases['out']
 
-#     print(len(text_list))
-#     print(len(trigger_list))
-#     print(len(type_list))
 
-    trigger_temp_list = []
-    type_temp_list = []
-    text_list_len = len(text_list)
+pred = RNN(x, istate, weights, biases)
 
-    ace_data = []
-    ace_data_labels = []
+# 定义损失和优化方法，其中算是为softmax交叉熵，优化方法为Adam
+cost = tf.reduce_mean(          # 5
+    tf.nn.softmax_cross_entropy_with_logits(pred, y))  # Softmax loss
+optimizer = tf.train.AdamOptimizer(
+    learning_rate=learning_rate).minimize(cost)  # Adam Optimizer
 
-    for i in range(text_list_len):
-        event_text = text_list[i]
-        trigger_temp_list = trigger_list[i]
-        type_temp_list = type_list[i]
+# 进行模型的评估，argmax是取出取值最大的那一个的标签作为输出
+correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
-        sentence_word2vec_arr = []  # 句子
-        trigger_labels = []
+# 初始化
+init = tf.global_variables_initializer()
+# init = tf.initialize_all_variables()  # 6
 
-        ace_text_list = NLPIR_ParagraphProcess(event_text, 0).split(' ')
-        for word in ace_text_list:
-            # 读取词向量，如果没有该单词，则None
-            try:
-                word_vector = model[word]
-            except KeyError:
-                word_vector = None
+# 开始运行
+with tf.Session() as sess:
+    sess.run(init)
+    step = 0
+    # 持续迭代
+    while step < training_iters:
 
-            #
-            if word_vector is not None:
-                sentence_word2vec_arr.append(word_vector)
-                if word in trigger_temp_list:
-                    val = ace_type_dict[
-                        type_temp_list[trigger_temp_list.index(word)]]
-                    a = [0.0 for x in range(0, 34)]
-                    a[val] = 1.0
-                    trigger_labels.append(a)
-                else:
-                    val = ace_type_dict['None-role']
-                    a = [0.0 for x in range(0, 34)]
-                    a[val] = 1.0
-                    trigger_labels.append(a)
+        batch_xs = ace_data_train[step]
+        batch_ys = ace_data_train_labels[step]
+        batch_size = len(batch_xs)
+        batch_xs = batch_xs.reshape([batch_size, n_steps, n_input])
+        # 迭代
+        sess.run(optimizer, feed_dict={x: batch_xs, y: batch_ys,
+                                       istate: np.zeros((batch_size, 2 * n_hidden))})
+        # 在特定的迭代回合进行数据的输出
+        if step % display_step == 0:
+            # Calculate batch accuracy
+            acc = sess.run(accuracy, feed_dict={x: batch_xs, y: batch_ys,
+                                                istate: np.zeros((batch_size, 2 * n_hidden))})
+            # Calculate batch loss
+            loss = sess.run(cost, feed_dict={x: batch_xs, y: batch_ys,
+                                             istate: np.zeros((batch_size, 2 * n_hidden))})
+            print("Iter " + str(step * batch_size) + ", Minibatch Loss= " + "{:.6f}".format(loss) +
+                  ", Training Accuracy= " + "{:.5f}".format(acc))
+        step += 1
+    print("Optimization Finished!")
+    # 载入测试集进行测试
+    length = len(ace_data_test)
+    for i in range(length):
+        test_len = len(ace_data_test[i])
+        test_data = ace_data_test[i].reshape(
+            (-1, n_steps, n_input))  # 8
+        test_label = ace_data_test_labels[i]
+        print("Testing Accuracy:", sess.run(accuracy, feed_dict={
+            x: test_data, y: test_label, istate: np.zeros((test_len, 2 * n_hidden))}))
 
-        ace_data.append(sentence_word2vec_arr)
-        ace_data_labels.append(trigger_labels)
 
-    ace_data = np.array(ace_data)
-    ace_data_labels = np.array(ace_data_labels)
-
-    print(ace_data)
-    print(ace_data_labels)
-
-#     # 取出每一个事件实体
-#     for ace_info in ace_train_list:
+# # print(ace_data_train[0])
+# # print(ace_data_train_labels)
 #
-#         if ace_info.text not in text_list:
-#             if trigger_temp_list:
-#                 pass
-#             else:
-#                 trigger_temp_list.append(ace_info.trigger)
-#             trigger_list.append(trigger_temp_list)
-#             trigger_temp_list = []
-#             text_list.append(ace_info.text)
-#         else:
-#             trigger_temp_list.append(ace_info.trigger)
+# # sys.exit(0)
 #
-#         print(text_list)
-#         print(trigger_list)
-#         print('-------------------------------')
-
-#         # if
-#         ace_text_list = NLPIR_ParagraphProcess(ace_info.text, 0).split(' ')
-#         for word in ace_text_list:
-#             # 读取词向量，如果没有该单词，则None
-#             try:
-#                 word_vector = model[word]
-#             except KeyError:
-#                 word_vector = None
+# # set random seed for comparing the two result calculations
+# tf.set_random_seed(1)
 #
-#             #
-#             if word_vector is not None:
-#                 pass
-#             else:
-#                 print(word)
-
-    # print(ace_text_list)
-    # print(ace_info.toString())
-
-    print("-----------------------end----------------------")
+# # hyperparameters
+# lr = 0.001
+# training_iters = len(ace_data_train)
+# batch_size = 1
+#
+# n_inputs = 200   # MNIST data input (img shape: 28*28)
+# n_steps = 1    # time steps
+# n_hidden_units = 128   # neurons in hidden layer
+# n_classes = 34      # MNIST classes (0-9 digits)
+#
+# # tf Graph input
+# x = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
+# istate = tf.placeholder("float", [None, 2 * n_hidden_units])
+# y = tf.placeholder(tf.float32, [None, n_classes])
+#
+# # Define weights
+# weights = {
+#     # (28, 128)
+#     'in': tf.Variable(tf.random_normal([n_inputs, n_hidden_units])),
+#     # (128, 10)
+#     'out': tf.Variable(tf.random_normal([n_hidden_units, n_classes]))
+# }
+# biases = {
+#     # (128, )
+#     'in': tf.Variable(tf.constant(0.1, shape=[n_hidden_units, ])),
+#     # (10, )
+#     'out': tf.Variable(tf.constant(0.1, shape=[n_classes, ]))
+# }
+#
+#
+# def RNN(X, istate, weights, biases):
+#     # hidden layer for input to cell
+#     ########################################
+#
+#     # transpose the inputs shape from
+#     # X ==> (128 batch * 28 steps, 28 inputs)
+#     X = tf.reshape(X, [-1, n_inputs])
+#
+#     # into hidden
+#     # X_in = (128 batch * 28 steps, 128 hidden)
+#     X_in = tf.matmul(X, weights['in']) + biases['in']
+#     # X_in ==> (128 batch, 28 steps, 128 hidden)
+#     X_in = tf.reshape(X_in, [-1, n_steps, n_hidden_units])
+#
+#     # cell
+#     ##########################################
+#
+#     # basic LSTM Cell.
+#     lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(
+#         n_hidden_units, forget_bias=1.0, state_is_tuple=False)
+#     # lstm cell is divided into two parts (c_state, h_state)
+#     #init_state = lstm_cell.zero_state(batch_size, dtype=tf.float32)
+#
+#     # You have 2 options for following step.
+#     # 1: tf.nn.rnn(cell, inputs);
+#     # 2: tf.nn.dynamic_rnn(cell, inputs).
+#     # If use option 1, you have to modified the shape of X_in, go and check out this:
+#     # https://github.com/aymericdamien/TensorFlow-Examples/blob/master/examples/3_NeuralNetworks/recurrent_network.py
+#     # In here, we go for option 2.
+#     # dynamic_rnn receive Tensor (batch, steps, inputs) or (steps, batch, inputs) as X_in.
+#     # Make sure the time_major is changed accordingly.
+#     outputs, final_state = tf.nn.dynamic_rnn(
+#         lstm_cell, X_in, initial_state=istate, time_major=False)
+#
+#     # hidden layer for output as the final results
+#     #############################################
+#     results = tf.matmul(final_state[1], weights['out']) + biases['out']
+#
+#     # # or
+#     # unpack to list [(batch, outputs)..] * steps
+#     # states is the last outputs
+# #     outputs = tf.unpack(tf.transpose(outputs, [1, 0, 2]))
+# #     results = tf.matmul(outputs[-1], weights['out']) + biases['out']
+#
+#     return results
+#
+#
+# pred = RNN(x, istate, weights, biases)
+# cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
+# train_op = tf.train.AdamOptimizer(lr).minimize(cost)
+#
+# correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+# accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+#
+# with tf.Session() as sess:
+#     # tf.initialize_all_variables() no long valid from
+#     # 2017-03-02 if using tensorflow >= 0.12
+#     #     if int((tf.__version__).split('.')[1]) < 12:
+#     #         init = tf.initialize_all_variables()
+#     #     else:
+#     init = tf.global_variables_initializer()
+#     sess.run(init)
+#     step = 0
+#     while step < training_iters:
+#
+#         batch_xs = ace_data_train[step]
+#         batch_ys = ace_data_train_labels[step]
+#         batch_size = len(batch_xs)
+#         print(batch_size)
+#         batch_xs = batch_xs.reshape([batch_size, n_steps, n_inputs])
+#         #         batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+#         #         batch_xs = batch_xs.reshape([batch_size, n_steps, n_inputs])
+#         sess.run([train_op], feed_dict={
+#             x: batch_xs,
+#             y: batch_ys,
+#             istate: np.zeros((batch_size, 2 * n_hidden_units))
+#         })
+#         if step % 20 == 0:
+#             print(sess.run(accuracy, feed_dict={
+#                 x: batch_xs,
+#                 y: batch_ys,
+#                 istate: np.zeros((batch_size, 2 * n_hidden_units))
+#             }))
+#         step += 1
+#
+#     print("Optimization Finished!")
+#     # 载入测试集进行测试
+# #     test_len = 128
+# #     test_data = mnist.test.images[:test_len].reshape(
+# #         (-1, n_steps, n_inputs))  # 8
+# #     test_label = mnist.test.labels[:test_len]
+# #     print("Testing Accuracy:", sess.run(accuracy, feed_dict={
+# #           x: test_data, y: test_label}))
