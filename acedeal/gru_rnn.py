10fb0549cd8e6b872baebd729b3badb20cee761b
chenbin <chenbkeep@163.com>
2017-02-18 14:53:31 +0800

    词性，位置信息特征进行rnn

--- acedeal/gru_rnn.py
+++ acedeal/gru_rnn.py
@@ -33,15 +33,15 @@ learningRate = 0.001
 training_iters = 16000
 batchSize = 1
 
-nInput = 223  # we want the input to take the 28 pixels
-nSteps = 1  # every 28
-nHidden = 128  # number of neurons for the RNN 64 128 256
-nClasses = 34  # this is MNIST so you know
+nInput = 223
+nSteps = 1 
+nHidden = 128
+nClasses = 34
 
 x = tf.placeholder('float', [None, nSteps, nInput])
 y = tf.placeholder('float', [None, nClasses])
 
-weights = tf.Variable(tf.random_normal([2 * nHidden, nClasses]))
+weights = tf.Variable(tf.random_normal([2 * nHidden+23, nClasses]))
 
 biases = tf.Variable(tf.random_normal([nClasses]))
 
@@ -60,40 +60,21 @@ def gru_RNN(x, weights, biases):
                                             dtype=tf.float32)
     
     x_back=tf.slice(x, [0,0,200],[-1,-1,23])[0]
-#  
-#     results = tf.matmul(outputs[-1], weights) + biases
-#     x = tf.transpose(x, [1, 0, 2])
-#     x = tf.reshape(x, [-1, nInput])
-#     
-#     x = tf.split(0, nSteps, x)
-#     x=tf.slice(x, [0,0,0],[-1,-1,200])
-#     x=tf.split(0, nSteps, x)
-#     
-#     gru_fw_cell = tf.nn.rnn_cell.GRUCell(nHidden)
-#     gru_bw_cell = tf.nn.rnn_cell.GRUCell(nHidden)
-#   
-#     outputs, _, _ = tf.nn.bidirectional_rnn(gru_fw_cell, gru_bw_cell, x,dtype=tf.float32)
-    return x_back
-#     x = tf.transpose(x, [1, 0, 2])
-#     x = tf.reshape(x, [-1, nInput])
-#     x = tf.split(0, nSteps, x)
-# 
-#     gru_fw_cell = tf.nn.rnn_cell.GRUCell(nHidden)
-#     gru_bw_cell = tf.nn.rnn_cell.GRUCell(nHidden)
-# 
-#     outputs, _, _ = tf.nn.bidirectional_rnn(gru_fw_cell, gru_bw_cell, x,
-#                                             dtype=tf.float32)
-# 
-#     results = tf.matmul(outputs[-1], weights) + biases
-#     return results
+    x_back=tf.split(0, nSteps, x_back)
+    
+    #concat_dim：0表示纵向，1表示行，2表示列
+    x_all=tf.concat(2, [outputs,x_back])
+    
+    results=tf.matmul(x_all[-1], weights) + biases
+    return results
 
 pred = gru_RNN(x, weights, biases)
 
 # optimization
 # create the cost, optimization, evaluation, and accuracy
 # for the cost softmax_cross_entropy_with_logits seems really good
-# cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
-# optimizer = tf.train.AdamOptimizer(learning_rate=learningRate).minimize(cost)
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learningRate).minimize(cost)
 
 # correctPred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
 # accuracy = tf.reduce_mean(tf.cast(correctPred, tf.float32))
@@ -111,33 +92,22 @@ with tf.Session() as sess:
         batch_size = len(batch_xs)
         batch_xs = batch_xs.reshape([batch_size, nSteps, nInput])
 
-        #sess.run(optimizer, feed_dict={x: batch_xs, y: batch_ys})
-        temp_pred=sess.run(pred,feed_dict={x: batch_xs})
-        print(temp_pred)
-        
-        print(len(temp_pred))
-        print(len(temp_pred[0]))
-        print(len(temp_pred[0][0]))
-        sys.exit()
+        sess.run(optimizer, feed_dict={x: batch_xs, y: batch_ys})
 
-#         if step % 100 == 0:
-#             sk = 0
-#             acck = 0
-#             predictionk, y_k = sess.run([tf.argmax(pred, 1), tf.argmax(y, 1)], feed_dict={x: batch_xs, y: batch_ys})
-#             for t in range(len(y_k)):
-#                 if y_k[t] != 33:
-#                     #                     logging.info(
-#                     #                         'actual:' + str(y_k[t]) + '\t predict:' + str(predictionk[t]))
-#                     #print(str(y_[t]) + '\t' + str(prediction[t]))
-#                     sk = sk + 1
-#                     if y_k[t] == predictionk[t]:
-#                         acck = acck + 1
-# 
-#             if sk != 0:
-#                 print("Iter " + str(k) + '-----------acc=' + str(acck / sk))
+        if step % 100 == 0:
+            sk = 0
+            acck = 0
+            predictionk, y_k = sess.run([tf.argmax(pred, 1), tf.argmax(y, 1)], feed_dict={x: batch_xs, y: batch_ys})
+            for t in range(len(y_k)):
+                if y_k[t] != 33:
+                    sk = sk + 1
+                    if y_k[t] == predictionk[t]:
+                        acck = acck + 1
+ 
+            if sk != 0:
+                print("Iter " + str(k) + '-----------acc=' + str(acck / sk))
         k += 1
     print('Optimization finished')
-    sys.exit()
 
     # 载入测试集进行测试
     length = len(ace_data_test)
@@ -149,12 +119,10 @@ with tf.Session() as sess:
 #     acc = 0
     for i in range(length):
         test_len = len(ace_data_test[i])
-        test_data = ace_data_test[i].reshape(
-            (-1, nSteps, nInput))  # 8
+        test_data = ace_data_test[i].reshape((-1, nSteps, nInput))  # 8
         test_label = ace_data_test_labels[i]
         # prediction识别出的结果，y_测试集中的正确结果
-        prediction, y_ = sess.run(
-            [tf.argmax(pred, 1), tf.argmax(y, 1)], feed_dict={x: test_data, y: test_label})
+        prediction, y_ = sess.run([tf.argmax(pred, 1), tf.argmax(y, 1)], feed_dict={x: test_data, y: test_label})
         for t in range(len(y_)):
             if prediction[t] != 33:
                 p_s = p_s + 1
@@ -165,7 +133,7 @@ with tf.Session() as sess:
                     pr_acc = pr_acc + 1
 
     print('----------------------------------------------------')
-    print(str(pr_acc) + '------------' + str(r_s))
+    print(str(p_s)+'------' + str(r_s)+ '------'+str(pr_acc))
     p = pr_acc / p_s
     r = pr_acc / r_s
     f = 2 * p * r / (p + r)
