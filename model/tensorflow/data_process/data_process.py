75834a7fc066fe18cb6e58d0edbd59645db9c918
陈斌 <chenbkeep@163.com>
2017-03-25 22:49:04 +0800

    update

--- model/tensorflow/data_process/data_process.py
+++ model/tensorflow/data_process/data_process.py
@@ -59,7 +59,7 @@ def str_replace_by_position(str,replacestr,start,end):
 """
 读取apf文件内容
 """
-def read_documnet(file_name,wordlist):
+def read_documnet(file_name,wordlist,phrase_posi_dict):
     # 获取apf文件位置
     apf_filename = file_name+'.apf.xml'
     apf_f=open(apf_filename, 'rb')
@@ -75,10 +75,6 @@ def read_documnet(file_name,wordlist):
 
     try:
         doc = etree.fromstring(apf_content)
-        trigger_list = []
-        sen_list = []
-        event_list = []
-
         start_end_type_list = []
 
         for i in doc.xpath("//event"):
@@ -88,100 +84,109 @@ def read_documnet(file_name,wordlist):
             event_type = i.xpath("./@TYPE")[0] + '.' + i.xpath("./@SUBTYPE")[0]
             event_num = type_index.EVENT_MAP[event_type]
 
-
             for anchor in cur_ele:
                 start = int(anchor.xpath("./charseq/@START")[0])
                 end = int(anchor.xpath("./charseq/@END")[0])+1
-                # print(start,end)
-                # print(sgm_content1[start:end])
                 trigger_str=anchor.xpath("./charseq/text()")[0]
-                #print(trigger_str,sgm_content1[start:end])
-                #print(anchor.xpath("./charseq/text()")[0])
                 if trigger_str!='Q&A':
                     assert trigger_str == sgm_content[start:end], "触发词不匹配"
 
+                #触发词为短语的情况
+                if '\n' in trigger_str:
+                    trigger_str=trigger_str.replace('\n',' ')
+
                 if ' ' in trigger_str:
-                    trigger_tmp1=trigger_str.replace(' ','_')
-                    trigger_tmp2 = trigger_str.replace(' ', '-')
-                    #sgm_content=sgm_content.replace(sgm_content[start:end],trigger_str)
-                    #print(trigger_str)
-                    # if 'assault_w'==trigger_tmp:
-                    #     print(file_name)
+                    trigger_tmp1=trigger_str.replace(' ','_')   #下划线，短语，有词向量
+                    trigger_tmp2 = trigger_str.replace(' ', '-')#中间线，短语，有词向量
                     if trigger_tmp1 in wordlist:
                         sgm_content = str_replace_by_position(sgm_content, trigger_tmp1, start, end)
-                        #print(trigger_tmp1)
                     elif trigger_tmp2 in wordlist:
                         sgm_content = str_replace_by_position(sgm_content, trigger_tmp2, start, end)
                     else:
-                        pass
+                        #如果二者都没有，选取其中某个词来做
+                        trigger_tmp3=trigger_str.replace('\n', ' ')
+                        if trigger_tmp3 in phrase_posi_dict.keys():
+                            new_trigger=trigger_tmp3.split(' ')[int(phrase_posi_dict[trigger_tmp3])-1]
+                            new_trigger_start=trigger_tmp3.find(new_trigger)
+                            start+=new_trigger_start
+                            end=start+len(new_trigger)
+                            #print(trigger_tmp3,new_trigger,start,end)
+                        else:
+                            continue
 
-        # print(sgm_content)
-
-                # trigger_str = anchor.xpath("./charseq/text()")[0].replace('\n', '')
-                # trigger_list.append(trigger_str)
-                # event_list.append(event_num)
-
-        # assert len(trigger_list) == len(sen_list), '触发词数目与句子数目不相等'
-        # assert len(trigger_list) == len(event_list), '触发词数目与事件类型数目不相等'
-
-        # trilen = len(trigger_list)
-        # for i in range(trilen):
-        #     # 触发词在事件句中的位置
-        #     tri_position = sen_list[i].index(trigger_list[i])
-        #     # 事件句在文章中的位置
-        #     sen_position = text_content.index(sen_list[i])
-        #
-        #     assert tri_position != -1, '（' + trigger_list[i] + '）不在（' + sen_list[i] + '）中'
-        #     assert sen_position != -1, '（' + sen_list[i] + '）不在（' + text_content + '）中'
-        #
-        #     # 触发词在文章中的位置
-        #     tri_start = tri_position + sen_position
-        #     tri_end = tri_start + len(trigger_list[i]) - 1
-        #
-        #     start_end_type_list.append((int(tri_start), int(tri_end), event_list[i]))
+                #print(trigger_str,start,end,event_num)
+                start_end_type_list.append((start,end,event_num))
+        #print(start_end_type_list)
+        return sgm_content,start_end_type_list
 
     except Exception as e:
         print(e)
         print(file_name, 'droped')
 
-    return
+    return None
 
+def content2vec(sgm_content,start_end_type_list,wordlist):
+    for (start,end,event_num) in start_end_type_list:
+        word=sgm_content[start:end].replace('\n',' ').lower()
+        if word in wordlist:
+            pass
+        else:
+            pass
+
+
+def str_process(ss):
+    strlist=ss.split(' ')
+    k=len(strlist)
+    s=strlist[0]
+    for i in range(1,k-1):
+        s=s+' '+strlist[i]
+    return s,strlist[k-1]
 
 if __name__ == '__main__':
+    # ss='1 2 3 4'
+    # a,b=str_process(ss)
+    # print(a)
+    # print(b)
 
-    # str='123 456'
-    # print(str_replace_by_position(str,'_',3,4))
+    # d = {'name':1,'age':2,'sex':3}
+    # if 'namea' in d.keys():
+    #     print('111')
+    # else:
+    #     print('222')
+    # # print(d['name'])
     # sys.exit()
 
-    filename1=acepath+'/nw/timex2norm\AFP_ENG_20030323.0020'
+    filename1=acepath+'/nw/timex2norm/AFP_ENG_20030323.0020'
     filename2=acepath+'/nw/timex2norm/AFP_ENG_20030509.0345'
     filename3 = acepath + '/bc/timex2norm/CNN_IP_20030330.1600.05-2'
     filename4 = acepath + 'bn/timex2norm/CNN_ENG_20030424_113549.11'
     filename5 = acepath + 'un/timex2norm/alt.corel_20041228.0503'
     filename6 = acepath + '/wl/timex2norm/AGGRESSIVEVOICEDAILY_20041226.1712'
     filename7 = acepath + '/cts/timex2norm/fsh_29191'
-    filename8=acepath+'bc/timex2norm/CNN_CF_20030304.1900.04'
-    # read_documnet(filename8)
-    # sys.exit()
+    filename8=acepath+'un/timex2norm/alt.obituaries_20041121.1339'
+
 
     wordlist_file = homepath + '/ace05/word2vec/wordlist'
     wordlist = [i.replace('\n', '') for i in open(wordlist_file, 'r')]
-    # for i in wordlist:
-    #     if 'took_on' in i:
-    #         print(i)
-    #     #print(i)
-    # # if 'took on' in wordlist:
-    # #     print('took on')
-    # sys.exit()
+    phrase_posi_file=homepath+'/ace05/word2vec/phrase_posi.txt'
+    phrase_posi_dict={}
+    for i in open(phrase_posi_file, 'r'):
+        a,b=str_process(i.replace('\n', ''))
+        phrase_posi_dict[a]=b
 
+    # sgm_content,start_end_type_list=read_documnet(filename8,wordlist,phrase_posi_dict)
+    # if content2vec(sgm_content,start_end_type_list,wordlist)==1:
+    #     print('111111111111111111111111')
+    # sys.exit()
 
     doclist=homepath+'/ace05/new_filelist_ACE_full.txt';
     f_list=[i.replace('\n','') for i in open(doclist,'r')]
     k=0
     for i in f_list:
         path=acepath+i
-        read_documnet(path,wordlist)
-        k=k+1
+        sgm_content,start_end_type_list=read_documnet(path,wordlist,phrase_posi_dict)
+        content2vec(sgm_content,start_end_type_list,wordlist)
+        k+=1
     print(k)
     #read_documnet(filename1)
 
