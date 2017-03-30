ee0007aefd4c94706a0b84b6fcff08cd28185b1d
chenbin <chenbkeep@163.com>
2017-03-30 17:47:04 +0800

    update

--- model/tensorflow/model1/cnn2.py
+++ model/tensorflow/model1/cnn2.py
@@ -1,41 +1,140 @@
 # coding:utf-8
 """
-cnn
-
+Created on 2017年3月20日
+事件识别  34分类
+rnn
+@author: chenbin
 """
 
 import tensorflow as tf
 import numpy as np
 import pickle
+import nltk
+import itertools
+import json
+import sys
+import time
 
-data_f = open('./enACEdata/train_data2.data', 'rb')
+data_f = open('../enACEdata/data2/train_data34.data', 'rb')
 X_train, Y_train, X_dev, Y_dev, X_test, Y_test = pickle.load(data_f)
 data_f.close()
 
-sess = tf.InteractiveSession()
+saver_path="../enACEdata/saver/checkpoint2.data"
 
-def conv2d(a,b):
-    return tf.nn.conv2d(a, b, strides=[1, 1, 1, 1], padding='SAME')
+# 参数
+event_num=12524
+learningRate = 0.03
+training_iters = event_num*10
+batch_size = 1
 
-def max_pool_2x2(a):
-    return tf.nn.max_pool(a, ksize=[1, 2, 2, 1],
-                          strides=[1, 2, 2, 1], padding='SAME')
+nInput = 300
+nSteps = 1
+nHidden = 100
+nClasses = 34
+
+x = tf.placeholder('float', [None, nSteps, nInput])
+y = tf.placeholder('float', [None, nClasses])
+
+seq_len = tf.placeholder(tf.int32, [None])
+
+weights = tf.Variable(tf.random_normal([2 * nHidden, nClasses]))
+
+biases = tf.Variable(tf.random_normal([nClasses]))
 
 
-sess.run(tf.initialize_all_variables())
+def cnn_rnn(x, weights, biases, seq_len):
+    x = tf.transpose(x, [1, 0, 2])
+    x = tf.reshape(x, [-1, nInput])
+    x = tf.reshape(x, [-1, nSteps, nInput])
+
+    lstm_fw_cell=tf.nn.rnn_cell.BasicLSTMCell(nHidden,forget_bias=2.0)
+    lstm_bw_cell=tf.nn.rnn_cell.BasicLSTMCell(nHidden,forget_bias=2.0)
+
+    lstm_fw_cell = tf.nn.rnn_cell.MultiRNNCell([lstm_fw_cell] * 2)
+    lstm_bw_cell = tf.nn.rnn_cell.MultiRNNCell([lstm_bw_cell] * 2)
+
+    outputs, output_states = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell, lstm_bw_cell, x,sequence_length=seq_len, dtype=tf.float32)
+
+    outputs = tf.concat(2, outputs)
+    last_output = outputs[:, 0, :]
+    results =tf.matmul(last_output, weights) + biases
+
+    return x,results,last_output
+
+pred = cnn_rnn(x, weights, biases, seq_len)
 
 
+# cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
+# optimizer = tf.train.GradientDescentOptimizer(learning_rate=learningRate).minimize(cost)
 
-# max_length=-1
-# for i in range(len(X_train)):
-#     max_length=max_length if max_length>len(X_train[i]) else len(X_train[i])
+
+# def compute_accuracy():
+#     # saver = tf.train.Saver(tf.global_variables())
+#     # saver.restore(sess, "../enACEdata/saver/checkpoint.data")
+#     # 载入测试集进行测试
+#     length = len(X_test)
+#     p_s = 0  # 识别的个体总数
+#     r_s = 0  # 测试集中存在个个体总数
+#     pr_acc = 0  # 正确识别的个数
+#     for i in range(length):
+#         test_len = len(X_test[i])
+#         test_data = np.array(X_test[i]).reshape((-1, nSteps, nInput))  # 8
+#         train_seq_len = np.ones(test_len) * nSteps
+#         test_label = Y_test[i]
+#         # prediction识别出的结果，y_测试集中的正确结果
+#         prediction, y_ = sess.run([tf.argmax(pred, 1), tf.argmax(y, 1)], feed_dict={x: test_data, y: test_label,seq_len:train_seq_len})
+#         for t in range(len(y_)):
+#             if prediction[t] != 33:
+#                 p_s +=1
 #
-# for i in range(len(X_dev)):
-#     max_length=max_length if max_length>len(X_dev[i]) else len(X_dev[i])
+#             if y_[t] != 33:
+#                 r_s +=1
+#                 if y_[t] == prediction[t]:
+#                     pr_acc +=1
 #
-# for i in range(len(X_test)):
-#     max_length=max_length if max_length>len(X_test[i]) else len(X_test[i])
+#     print('----------------------------------------------------')
+#     print(str(pr_acc) + '------'+str(p_s)+'------' + str(r_s))
+#     p = pr_acc / p_s
+#     r = pr_acc / r_s
+#     if p+r!=0:
+#         f = 2 * p * r / (p + r)
+#         print('P=' + str(p) + "\tR=" + str(r) + "\tF=" + str(f))
+#         return f
 #
-# print(max_length)
+#     return 0
 
+
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(init)
+
+    k = 0
+    max_f=0
+    while k < training_iters:
+        step = k % event_num
+
+        batch_xs = X_train[step]
+        batch_ys = Y_train[step]
+        batch_size = len(batch_xs)
+        batch_xs = np.array(batch_xs).reshape([batch_size, nSteps, nInput])
+
+        train_seq_len = np.ones(batch_size) * nSteps
+
+        x,results, last_output=sess.run(pred, feed_dict={x: batch_xs, y: batch_ys,seq_len:train_seq_len})
+        print(np.array(x).shape)
+        print(np.array(results).shape)
+        print(np.array(last_output).shape)
+        print(x)
+        # print(last_output)
+        sys.exit()
+
+        # if k!=0 and step==0:
+        #
+        #     f=compute_accuracy()
+        #     if f>max_f:
+        #         saver = tf.train.Saver(tf.global_variables())
+        #         saver.save(sess,saver_path)
+        #         max_f=f
+        k += 1
+    print('Optimization finished')
 
--- model/tensorflow/model1/cnn_test.py
+++ model/tensorflow/model1/cnn_test.py
@@ -1,7 +1,6 @@
 import numpy as np
 import tensorflow as tf
 
-
 def conv2d_test():
     #Input: x
     x_image = tf.placeholder(tf.float32,shape=[5,5])
@@ -43,23 +42,15 @@ def conv2d_test():
         print(y.reshape(3,3))
 
 
-def max_pool_test():
-    #input:x
-    x_image = tf.placeholder(tf.float32,shape=[4,4])
-    x = tf.reshape(x_image,[1,4,4,1])
+def max_pool_test(x_data):
+    (width, height) = x_data.shape
 
-    #ksize & Stride & padding
-    ksize = [1,2,2,1]
-    strides=[1,2,2,1]
-    padding = 'VALID'
+    #input:x
+    x_image = tf.placeholder(tf.float32,shape=[width,height])
+    x = tf.reshape(x_image,[1,width,height,1])
 
     #max pooling
-    y = tf.nn.max_pool(x,ksize,strides,padding)
-
-    x_data = np.array([    [4,3,1,8]
-                          ,[7,2,6,3]
-                          ,[2,0,1,1]
-                          ,[3,4,2,5]],dtype = np.float32)
+    y = tf.nn.max_pool(x,[1,1,height,1],[1,1,1,1],'VALID')
 
     with tf.Session() as sess:
         init = tf.global_variables_initializer()
@@ -69,16 +60,81 @@ def max_pool_test():
         y = (sess.run(y,feed_dict={x_image:x_data}))
 
         print("The shape of x:\t", x.shape, ",\t and the x.reshpe(4,4) is :")
-        print(x.reshape(4,4))
+        print(x.reshape(width,height))
 
         print("The shape of y:\t", y.shape, ",\t and the y.reshpe(2,2) is :")
-        print(y.reshape(2,2))
+        (a,b,c,d)=y.shape
+        print(y.reshape(b,c))
+
+
+def conv2d_max_pool(x_data):
+
+    (width,height)=x_data.shape
+
+    x_image = tf.placeholder(tf.float32,shape=[width,height])
+    x = tf.reshape(x_image,[1,width,height,1])
+
+    #Filter: W
+    W_cpu = np.array([[1,1,1],[1,1,1],[1,1,1]],dtype=np.float32)
+    W = tf.Variable(W_cpu)
+    W = tf.reshape(W, [3,3,1,1])
+
+    #Convolution  Stride & Padding
+    con2d_result = tf.nn.conv2d(x, W, [1, 1, 1, 1], 'VALID')
+    con2d_result=tf.reshape(con2d_result,[1,width-2,height-2,1])
+
+    max_pool_result = tf.nn.max_pool(con2d_result, [1,1,height-2,1], [1,1,1,1], 'VALID')
+    max_pool_result=tf.reshape(max_pool_result,[width-2])
+
+    with tf.Session() as sess:
+        init = tf.global_variables_initializer()
+        sess.run(init)
+
+        x = (sess.run(x, feed_dict={x_image: x_data}))
+        W = (sess.run(W, feed_dict={x_image: x_data}))
+        con2d_result = (sess.run(con2d_result, feed_dict={x_image: x_data}))
+        max_pool_result= (sess.run(max_pool_result, feed_dict={x_image: x_data}))
+
+        print("The shape of x:\t", x.shape, ",\t and the x.reshape(5,5) is :")
+        print(x.reshape(width,height))
+
+        print("The shape of x:\t", W.shape, ",\t and the W.reshape(3,3) is :")
+        print(W.reshape(3,3))
+
+        print("The shape of y:\t", con2d_result.shape, ",\t and the y.reshape is :")
+        print(con2d_result.reshape(width-2,height-2))
+
+        print("The shape of y:\t", max_pool_result.shape, ",\t and the y.reshape is :")
+        # (a, b, c, d) = max_pool_result.shape
+        print(max_pool_result)
+
+    return 0
 
 
 if __name__ == '__main__':
-    print('-------------------conv2d_test--------------------')
-    conv2d_test()
 
-    print('------------------max_pool_test---------------------')
-    max_pool_test()
+    # x=[i for i in range(3300)]
+    # x=np.array(x).reshape(300,11)
+    # print(x.shape)
+    # print(x)
+    # print('----------------------------------------')
+
+    x_tmp=[i+1 for i in range(3300)]
+    x_data = np.array([x_tmp],dtype=np.float32).reshape(300, 11)
+    print(x_data.shape)
+
+    result=conv2d_max_pool(x_data)
+
+
+    # print('-------------------conv2d_test--------------------')
+    # conv2d_test()
+    #
+
+    # x_tmp=[i+1 for i in range(2682)]
+    # x_data = np.array([x_tmp],dtype=np.float32).reshape(298, 9)
+    # x_tmp=[i+1 for i in range(28)]
+    # x_data = np.array([x_tmp],dtype=np.float32).reshape(4, 7)
+    # print(x_data.shape)
+    # print('------------------max_pool_test---------------------')
+    # max_pool_test(x_data)
 
